f233fbd9c11c2fbd7a438df5c37f8e50585ce40b
Boaz Stolk <bstolk@aweta.nl>
2015-05-01 10:36:09 +0200

    rename generate executable

--- src/xor/generate.sh
+++ src/xor/generate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-../../build/src/xor/generate-random-training-data --backend=lmdb --split=1 --shuffle=true 1000 xor_lmdb
+../../build/src/xor/generate-random-xor-training-data --backend=lmdb --split=1 --shuffle=true 1000 xor_lmdb
