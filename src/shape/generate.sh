7db173938db20f6a5bff3abd0dff156de11bc207
Boaz Stolk <boaz.stolk@gmail.com>
2016-10-16 14:57:30 +0200

    remove old lmdb dir before generating

--- src/shape/generate.sh
+++ src/shape/generate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+rm -r shape_lmdb_test
+rm -r shape_lmdb_train
 ../../build/src/shape/generate-random-shape-training-data --backend=lmdb --split=1 --shuffle=true --balance=true shape_lmdb
--- src/xor/generate.sh
+++ src/xor/generate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+rm -r xor_lmdb_test
+rm -r xor_lmdb_train
 ../../build/src/xor/generate-random-xor-training-data --backend=lmdb --split=1 --shuffle=true 1000 xor_lmdb
